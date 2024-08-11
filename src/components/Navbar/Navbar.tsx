c3c5109fe881cfa6291e6298c9def4d684c4114c
Pasha Rezero <kivachuk.2018@gmail.com>
2024-08-11 23:15:59 +0300

    update navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,39 @@
 import { Link } from "react-router-dom";
+import { useUser } from "../../contexts/user/useUser";
+import supabase from "../../utils/supabaseClient";
 
 export default function Navbar() {
+  const { user } = useUser();
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="bg-[#808080] text-white w-full h-[60px]">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto p-4 flex justify-between items-center">
         <div className="text-lg font-bold">
           <Link to="/">Менеджер задач</Link>
         </div>
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-        </ul>
+        {!user ? (
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        ) : (
+          <div className="flex space-x-4">
+            <p>{user.user_metadata.username}</p>
+            <button onClick={() => signOut()} className="text-red-500">
+              Logout
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
