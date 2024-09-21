92ce7bebe2912e87d6340236f4f619a90117a675
Pasha Rezero <kivachuk.2018@gmail.com>
2024-09-21 17:03:09 +0300

    add discord,github auth

--- src/components/Auth/AuthComponent.tsx
+++ src/components/Auth/AuthComponent.tsx
@@ -3,11 +3,15 @@ import supabase from "../../utils/supabaseClient";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import GoogleIcon from "@mui/icons-material/Google";
+import { FaDiscord } from "react-icons/fa";
 import { InputComponent } from "../CustomInput/InputComponent";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 type AuthComponentProps = {
   url: string;
 };
+
+type Provider = "google" | "discord" | "github";
 
 export const AuthComponent = ({ url }: AuthComponentProps) => {
   const isSignIn = url.includes("/login");
@@ -60,19 +64,27 @@ export const AuthComponent = ({ url }: AuthComponentProps) => {
     }
   };
 
-  const handleGoogleSignIn = async () => {
+  const handleSocialSignIn = async (provider: Provider) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: "google",
+      provider: provider,
     });
-    if (error) console.error("Google sign in error:", error.message);
+    if (error) console.error("Social sign in error:", error.message);
     if (data) navigate("/");
   };
 
   return (
     <div className="min-h-screen bg-gray-800">
       <div className="bg-gray-700 w-[330px] max-h-[420px] mx-auto text-white rounded-xl p-4">
-        <div className="bg-[#8D8D9C] w-[30px] h-[30px] mx-auto rounded-xl flex items-center justify-center">
-          <GoogleIcon onClick={handleGoogleSignIn} />
+        <div className="flex justify-center align-center gap-4">
+          <div className="bg-[#8D8D9C] w-[30px] h-[30px] rounded-xl flex items-center justify-center">
+            <GoogleIcon onClick={() => handleSocialSignIn("google")} />
+          </div>
+          <div className="bg-[#6c6c96] w-[30px] h-[30px] rounded-xl flex items-center justify-center">
+            <FaDiscord onClick={() => handleSocialSignIn("discord")} />
+          </div>
+          <div className="bg-black w-[30px] h-[30px] rounded-xl flex items-center justify-center">
+            <GitHubIcon onClick={() => handleSocialSignIn("github")} />
+          </div>
         </div>
         <h1 className="text-3xl text-center my-4">
           {isSignIn ? "Sign In" : "Sign Up"}
