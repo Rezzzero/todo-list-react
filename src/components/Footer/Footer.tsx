a976ae9ae83231300f7a45fcb16af606e8afa195
Pasha Rezero <kivachuk.2018@gmail.com>
2024-10-04 14:58:14 +0300

    don't testing styles

--- src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <div className="w-full h-20 bg-[#28292c] text-white">
-      <div className="container mx-auto flex justify-center items-center">
-        Footer
-      </div>
-    </div>
-  );
-};
